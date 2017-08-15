096deb860a111ab88dbbf1b9e7000d7399dda1b0
Marco Biedermann <marco.biedermann@gmx.de>
2017-08-15 23:59:26 +0200

    install apache2

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,3 +5,17 @@ sudo -s
 # Update OS
 apt-get -y update
 apt-get -y upgrade
+
+# Install Apache
+apt-get -y install apache2
+
+if ! [ -L /var/www ]; then
+  rm -rf /var/www
+  ln -fs /vagrant /var/www
+fi
+
+# Enable Apache Modules
+a2enmod rewrite
+
+# Restart Apache Server
+service apache2 restart
